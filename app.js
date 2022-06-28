987ff6e493a03fd883a33e0cb284c66e20b5bead
Michael Haraeb <michael.haraeb@aquare.la>
2022-06-28 09:37:29 -0300

    Added Procfile for Heroku deployment

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ app.post("/", function (req, res) {
 
 // console.log(firstName, lastName, email);
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("Server listening on port 3000");
 });
 
